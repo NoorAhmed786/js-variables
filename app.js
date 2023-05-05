6e5085d6c488fa70a2fca0e8148716af8cbe4e45
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-06 00:52:28 +0500

    assigment 2

---
+++ app.js
@@ -0,0 +1,42 @@
+//qustn 1
+var usernmae="noor";
+ 
+//qustn 2
+ var myName="noor";
+
+//qustn 3
+
+var message="Hello World!";
+alert(message);
+
+//qustn 4
+ var studentsName="noor";
+ var studentsAge="15 yaers old";
+ var studentsDegree="certified mobile and app application developer";
+ alert(studentsName);
+ alert(studentsAge);
+ alert(studentsDegree);
+
+ //qustn 5
+ var abc="PIZZA \nPIZZ\nPIZ\nPI\nP";
+ alert(abc);
+
+ //qustn 6
+ var email1="my eamil is";
+ var email2="noorahmed123@gmail.com";
+ var email3= email1 + " "+ email2;
+ alert(email3);
+
+ //qustn 7
+ var book1="I am trying to learn from the Book";
+ var book2="A Smarter Way to laern javascript";
+ var book3= book1 + " " +book2;
+ alert(book3);
+
+
+ //qustn 8
+ document.write("Yah ! i can write HTML content through javascript");
+
+ //qustn 9
+ var symbol="=====&$*@*$&=====";
+ alert(symbol);
